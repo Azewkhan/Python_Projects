0e62ee73f7fa92040f110838265795e76d2e6ba2
Azewkhan <mohammadazeemk75@gmail.com>
2026-03-11 11:59:05 -0400

    Day 3

---
+++ Advanced/Use_PhoneCamera_with_python_.py
@@ -0,0 +1,12 @@
+import cv2
+import numpy as np
+url = "Your IP address/video"   # Here fill Your IP address from your IP webcam app after starting the server from top right corner
+cp = cv2.VideoCapture(url)   # If we type 0 insteadof URL in here It will show laptop webcam 
+while(True):
+    camera, frame = cp.read()
+    if frame is not None:
+        cv2.imshow("Frame", frame)
+    q = cv2.waitKey(1)
+    if q==ord("q"):
+        break
+cv2.destroyAllWindows()
